f5cfefc3a3784f25a2826f113fd1d97c232863c2
Rodrigo-Brizuela <rodrigo8935@gmail.com>
2023-01-04 20:09:54 -0300

    Focus y value vacio en input, y btn eliminar

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
 const div_tareas = document.getElementById("div_tareas");
+const btn_agregarTarea = document.getElementById("btn_agregarTarea");
+
+btn_agregarTarea.addEventListener(`click`,agregarTarea);
 
 function agregarTarea (){
     const card = document.createElement("div");
-    const input_tarea = document.getElementById("input_tarea").value;
+
+    const input_tarea = document.getElementById("input_tarea");
+    const tarea = input_tarea.value;
+
     const text_tarea = document.createElement("h6");
+
+    const btn_eliminar = document.createElement("button");
     
-    if (input_tarea !== ""){
-        text_tarea.textContent = input_tarea;
+    if (tarea !== ""){
+        text_tarea.textContent = tarea;
 
         card.className = "notes";
+        btn_eliminar.textContent = "Eliminar";
+        btn_eliminar.className = "btn_eliminar";
+        btn_eliminar.addEventListener(`click`, () => eliminarTarea(card))
 
         card.appendChild(text_tarea);
+        card.appendChild(btn_eliminar);
         div_tareas.appendChild(card);
     } else{
         alert("Debe ingresar una tarea!")
     }
 
-    
+
+    input_tarea.value= "";
+    input_tarea.focus();
+}
+
+function eliminarTarea(card) {
+    div_tareas.removeChild(card);
 }
