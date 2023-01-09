a02d294cbdc0442bd51ac1045789c0d04d2b6b43
rodrigo <rodrigo8935@gmail.com>
2023-01-09 12:16:01 -0300

    Responsive cards, estilo scroll, title styles, font adds

--- index.js
+++ index.js
@@ -4,24 +4,26 @@ const btn_agregarTarea = document.getElementById("btn_agregarTarea");
 btn_agregarTarea.addEventListener(`click`,agregarTarea);
 
 function agregarTarea (){
-    const card = document.createElement("div");
-    const div_p = document.createElement("div");
-    const div_btn = document.createElement("div");
-
     const input_tarea = document.getElementById("input_tarea");
     const tarea = input_tarea.value;
-
-    const text_tarea = document.createElement("p");
-
-    const btn_eliminar = document.createElement("button");
     
     if (tarea !== ""){
+        const card = document.createElement("div");
+        const div_p = document.createElement("div");
+        const div_btn = document.createElement("div");
+        const class_card = "col-6 col-sm-6 col-md-4 col-lg-3 notes";
+
+        const text_tarea = document.createElement("p");
+
+        const btn_eliminar = document.createElement("button");
+    
+    
         text_tarea.textContent = tarea;
         text_tarea.className = "p_tarea";
         div_p.className = "div_p";
         div_btn.className = "div_btn";
-
-        card.className = "notes";
+        
+        card.className = class_card;
         btn_eliminar.textContent = "Eliminar";
         btn_eliminar.className = "btn_eliminar";
         btn_eliminar.addEventListener(`click`, () => eliminarTarea(card))
@@ -34,7 +36,6 @@ function agregarTarea (){
     } else{
         alert("Debe ingresar una tarea!")
     }
-
 
     input_tarea.value= "";
     input_tarea.focus();
